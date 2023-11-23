a925d62fbd123fb23845ab01f8bfd1a9e2b41b25
caio.coelho <caio.coelho@totvs.com.br>
2023-11-23 12:27:48 -0300

    Testes

--- src/main/java/com/titan/auth/sys/core/telefoneclinica/domain/TelefoneClinica.java
+++ src/main/java/com/titan/auth/sys/core/telefoneclinica/domain/TelefoneClinica.java
@@ -1,14 +1,12 @@
 package com.titan.auth.sys.core.telefoneclinica.domain;
 
 import com.titan.auth.sys.core.infra.config.domain.entity.AbstractAuditableAggregate;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import lombok.NonNull;
 
 import java.util.UUID;
 
@@ -23,7 +21,7 @@ public class TelefoneClinica extends AbstractAuditableAggregate {
 	private UUID clinicaId;
 	private String telefone;
 
-	public static TelefoneClinica registrarTelefoneClinica(@NonNull UUID clinicaId, @NonNull String telefone) {
+	public static TelefoneClinica registrarTelefoneClinica(UUID clinicaId, String telefone) {
 		return new TelefoneClinica(
 				UUID.randomUUID(),
 				clinicaId,
