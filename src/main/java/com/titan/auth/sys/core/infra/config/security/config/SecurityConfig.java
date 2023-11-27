8d4c55bf3be4da1af11ddceba7bfc174bb175112
caio.coelho <caio.coelho@totvs.com.br>
2023-11-27 07:58:05 -0300

    Cors

--- src/main/java/com/titan/auth/sys/core/infra/config/security/config/SecurityConfig.java
+++ src/main/java/com/titan/auth/sys/core/infra/config/security/config/SecurityConfig.java
@@ -7,16 +7,16 @@ import com.titan.auth.sys.core.infra.config.security.filter.JwtAuthFilter;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.OncePerRequestFilter;
-
-import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
@@ -34,14 +34,14 @@ public class SecurityConfig {
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http.authorizeHttpRequests(
-						(authz) ->
+						authz ->
 								authz
 										.requestMatchers("api/auth").permitAll()
 										.requestMatchers("api/v1/**").authenticated()
 										.anyRequest().permitAll()
 				)
 				.csrf(AbstractHttpConfigurer::disable)
-				.cors(withDefaults())
+				.cors(cors -> cors.configurationSource(this.corsConfigurationSource()))
 				.httpBasic(AbstractHttpConfigurer::disable)
 				.sessionManagement(httpSecuritConfigurer -> httpSecuritConfigurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 				.addFilterBefore(this.jwtFilter(), UsernamePasswordAuthenticationFilter.class)
@@ -50,4 +50,15 @@ public class SecurityConfig {
 		return http.build();
 	}
 
+	public CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration corsConfiguration = new CorsConfiguration();
+		corsConfiguration.addAllowedHeader("*");
+		corsConfiguration.addAllowedMethod("*");
+		corsConfiguration.addAllowedOrigin("*");
+
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", corsConfiguration);
+
+		return source;
+	}
 }
